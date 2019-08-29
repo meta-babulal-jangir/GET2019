fbefb2ae021815072489921a10b9c8eb2bf2280f
meta-babulal-jangir <babulal.jangir@metacube.com>
2019-08-29 19:13:32 +0530

    EAD/ASSIGNMENT2

---
+++ EAD/ASSIGNMENT2/src/com/servlets/addStudentDetail.java
@@ -0,0 +1,34 @@
+package com.servlets;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+
+/**
+ * @author Babulal Jangir
+ * Servlet to add Student data into database
+ */
+public class addStudentDetail extends HttpServlet {
+	public void doGet (HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.println("In the servlet are following");
+		Student student=new Student();
+		student.setFirstName(req.getParameter("first_name"));
+		student.setLastName(req.getParameter("last_name"));
+		student.setFatherName(req.getParameter("father_name"));
+		student.setEmail(req.getParameter("email_id"));
+		student.setStudentClass(Integer.parseInt(req.getParameter("class")));
+		student.setAge(Integer.parseInt(req.getParameter("age")));
+		boolean added=DatabaseHelper.addStudent(student);
+		if(added)
+			out.println("Added Sucessfully");
+		else
+			out.println("Not Added");
+	}
+}
